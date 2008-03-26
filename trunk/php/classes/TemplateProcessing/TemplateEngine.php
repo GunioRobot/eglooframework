44dfd9727522347dee7f5075441c4750fcc1414d
egd0000 <egd0000@caf7fd9c-7546-0410-8e03-b3d1908ba6f3>
2008-03-26 00:07:28 +0000

    Committing initial revisions of template director and engine files
    git-svn-id: http://eglooframework.svn.sourceforge.net/svnroot/eglooframework@16 caf7fd9c-7546-0410-8e03-b3d1908ba6f3

---
+++ trunk/php/classes/TemplateProcessing/TemplateEngine.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * TemplateEngine Class File
+ *
+ * Contains the class definition for the TemplateEngine, a subclass of 
+ * the Smarty engine template class.
+ * 
+ * Copyright 2008 eGloo, LLC
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *  
+ * @author George Cooper
+ * @copyright 2008 eGloo, LLC
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ * @package Template
+ * @version 1.0
+ */
+
+include( 'Smarty/Smarty.class.php' );
+
+/**
+ * TemplateEngine
+ * 
+ * Provides a class definition for a generic template engine subclass of
+ * the Smarty template class.
+ *
+ * @package Template
+ */
+class TemplateEngine extends Smarty {
+
+    public function __construct( $install, $local ) {
+        $this->Smarty();
+        $this->left_delimiter = '<!--{'; 
+        $this->right_delimiter = '}-->'; 
+        
+        
+        $this->plugins_dir = array( 'plugins', '../php/classes/components' );
+        
+        $this->template_dir = '../templates';
+        $this->config_dir   = '../config/smarty';
+        $this->compile_dir  = '/data/eGloo/' . $install . '/' . 
+                                $local . '/cache/compiled_templates';
+        $this->cache_dir    = '/data/eGloo/' . $install . '/' .
+                                $local . '/cache/smarty_cache';
+        //$this->cache_handler_func = 'smarty_cache_memcache';
+        //$this->caching      = true;
+        $this->caching = false;
+    }
+    
+}
+
+?>
