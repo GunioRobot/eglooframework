28631625e4842e5bb36e5c67a0149b34b213c8a9
egd0000 <egd0000@caf7fd9c-7546-0410-8e03-b3d1908ba6f3>
2008-03-30 02:58:27 +0000

    Correcting file paths
    git-svn-id: http://eglooframework.svn.sourceforge.net/svnroot/eglooframework@56 caf7fd9c-7546-0410-8e03-b3d1908ba6f3

--- trunk/Test/Unit Tests/RequestProcessorTestCase.php
+++ trunk/Test/Unit Tests/RequestProcessorTestCase.php
@@ -6,7 +6,7 @@
  * Window - Preferences - PHPeclipse - PHP - Code Templates
  */
 
-require_once ( "../php/classes/RequestProcessor.php" );
+require_once ( "../PHP/Classes/RequestProcessor.php" );
 require_once ( "PHPUnit/Framework/TestCase.php" );
 
 class RequestProcessorTestCase extends PHPUnit_Framework_TestCase {
