05439f08494e6781b7653f64a49313a048a7c074
egd0000 <egd0000@caf7fd9c-7546-0410-8e03-b3d1908ba6f3>
2008-03-30 20:58:44 +0000

    Committing initial revisions of RequestProcessor and RequestProcessorFactory classes
    git-svn-id: http://eglooframework.svn.sourceforge.net/svnroot/eglooframework@72 caf7fd9c-7546-0410-8e03-b3d1908ba6f3

---
+++ trunk/PHP/Classes/RequestProcessing/RequestProcessor.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * RequestProcessor Abstract Class File
+ *
+ * Needs to be commented
+ * 
+ * Copyright 2008 eGloo, LLC
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *  
+ * @author Keith Buel
+ * @copyright 2008 eGloo, LLC
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ * @package RequestProcessing
+ * @version 1.0
+ */
+
+/**
+ * RequestProcessor
+ *
+ * Needs to be commented
+ * 
+ * @package RequestProcessing
+ */
+abstract class RequestProcessor {
+ 	
+ 	protected $requestInfoBean = null;
+	abstract public function processRequest();
+ 	
+    // TODO we need to make a templated method for processing
+    // both the header information and then the content information
+    // to guarantee that by default we handle content headers and
+    // and other prerequisites correctly 
+    
+ 	public function setRequestInfoBean( $requestInfoBean ){
+ 		$this->requestInfoBean = $requestInfoBean;
+ 	}
+ 	
+ 	public function getRequestInfoBean(){
+ 		return $this->requestInfoBean;
+ 	}
+ }
+ 
+?>
---
+++ trunk/PHP/Classes/RequestProcessing/RequestProcessorFactory.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * RequestProcessorFactory Final Class File
+ *
+ * Contains the RequestProcessorFactory class definition
+ * 
+ * Copyright 2008 eGloo, LLC
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *  
+ * @author Keith Buel
+ * @copyright 2008 eGloo, LLC
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ * @package RequestProcessing
+ * @version 1.0
+ */
+
+/**
+ * RequestProcessorFactory
+ *
+ * This class creates concrete request processors to process each type of request
+ * 
+ * @package RequestProcessing
+ */
+final class RequestProcessorFactory {
+
+    public static function getRequestProcessor( $requestInfoBean ) {
+
+        $requestProcessorID = $requestInfoBean->getRequestProcessorID();
+        $requestProcessor = null;
+
+        $cacheGateway = CacheGateway::getCacheGateway();
+        
+        if ( ( $requestProcessor = $cacheGateway->getObject( (string) $requestProcessorID, '<type>' ) ) == null ) {
+
+            if ( $requestProcessorID !== null ) {
+            	
+				//first make the concrete request processor
+            	$requestProcessor = eval( "return new $requestProcessorID();" );
+
+				//now add the decorators
+				foreach( $requestInfoBean->getDecoratorArray() as $decoratorID ){
+					
+					$requestDecorator = eval( "return new $decoratorID();" );
+					$requestDecorator->setChildRequestProcessor( $requestProcessor );
+					
+					//now set this decorator as the request processor
+					$requestProcessor = $requestDecorator;
+					
+				}            	
+            	
+            } else {
+                // TODO This shouldn't blindly return the main page because of our use of Ajax
+                // We'll have to do extensive checking on the referral.  If it's a failure
+                // on single component in the system, then we don't necessarily want to reload 
+                // the entire interface.  Just present a simple error message to the user, but
+                // don't reload their whole page.  
+                            	
+            	//$evalString = "return new ExternalMainPageBaseRequestProcessor();";
+                $requestProcessorID = "ExternalMainPageBaseRequestProcessor";
+            	$requestProcessor = new ExternalMainPageBaseRequestProcessor();
+            }
+
+            $cacheGateway->storeObject( (string) $requestProcessorID, $requestProcessor, '<type>' );
+        }
+        
+        $requestProcessor->setRequestInfoBean( $requestInfoBean );
+
+        return $requestProcessor;
+    }
+
+}
+ 
+?>
