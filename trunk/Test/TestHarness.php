55d2a6bc54e8a39e8c376c42d775f7f30d0d526b
egd0000 <egd0000@caf7fd9c-7546-0410-8e03-b3d1908ba6f3>
2008-03-31 00:11:31 +0000

    Commenting TestHarness class and testing CIA commit stat updates
    git-svn-id: http://eglooframework.svn.sourceforge.net/svnroot/eglooframework@81 caf7fd9c-7546-0410-8e03-b3d1908ba6f3

--- trunk/Test/TestHarness.php
+++ trunk/Test/TestHarness.php
@@ -1,6 +1,39 @@
 <?php
+/**
+ * TestHarness Class File
+ *
+ * Needs to be commented
+ * 
+ * Copyright 2008 eGloo, LLC
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *  
+ * @author George Cooper
+ * @copyright 2008 eGloo, LLC
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ * @package Testing
+ * @version 1.0
+ */
+
 require_once( "PHPUnit/Framework/TestSuite.php" );
 
+/**
+ * TestHarness
+ * 
+ * Needs to be commented
+ *  
+ * @package Testing
+ */
 class TestHarness extends PHPUnit_Framework_TestSuite {
     private $seen = array();
     
