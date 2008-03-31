40261705205d71efd488bac6e95b5721ab8084e9
egd0000 <egd0000@caf7fd9c-7546-0410-8e03-b3d1908ba6f3>
2008-03-31 07:59:48 +0000

    Committing initial revisions of public browse and ranking request processors
    git-svn-id: http://eglooframework.svn.sourceforge.net/svnroot/eglooframework@85 caf7fd9c-7546-0410-8e03-b3d1908ba6f3

---
+++ trunk/PHP/Classes/RequestProcessing/PublicBrowse/ViewUpdatedBlogProfilesRequestProcessor.php
@@ -0,0 +1,137 @@
+<?php
+/**
+ * ViewUpdatedBlogProfilesRequestProcessor Class File
+ *
+ * Needs to be commented
+ * 
+ * Copyright 2008 eGloo, LLC
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *  
+ * @author <UNKNOWN>
+ * @copyright 2008 eGloo, LLC
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ * @package RequestProcessing
+ * @version 1.0
+ */
+
+/**
+ * ViewUpdatedBlogProfilesRequestProcessor
+ * 
+ * Needs to be commented
+ * 
+ * @package RequestProcessing
+ * @subpackage RequestProcessors
+ */
+class ViewUpdatedBlogProfilesRequestProcessor extends RequestProcessor {
+    
+    public function processRequest() {
+        $templateDirector = TemplateDirectorFactory::getTemplateDirector( $this->requestInfoBean );
+        $templateBuilder = new XHTMLBuilder();
+        $templateDirector->setTemplateBuilder( $templateBuilder );
+        $templateDirector->preProcessTemplate();
+    			
+   		$mainProfileID = $_SESSION['MAIN_PROFILE_ID'];
+		$viewingProfileID = $_SESSION['MAIN_PROFILE_ID'];
+		$viewingUserProfileName = $_SESSION['USER_USERNAME'];
+		$loggedInUser = false;
+
+		// TODO check constraints on these values (ranges and types)
+		if ( $this->requestInfoBean->issetGET( 'startIndex' ) ) {
+			$startIndex = $this->requestInfoBean->getGET( 'startIndex' ); 
+		} else {
+			$startIndex = '0';
+		}
+		
+		if ( $this->requestInfoBean->issetGET( 'length' ) ) {
+			$length = $this->requestInfoBean->getGET( 'length' ); 
+		} else {
+			$length = '30';
+		}
+ 
+		$daoFunction = 'getRecentUpdateBlogProfiles';
+		$inputValues = array();
+		$inputValues[ 'start_index' ] = $startIndex;
+		$inputValues[ 'profile_count' ] = $length;
+		
+		$daoFactory = DAOFactory::getInstance();
+		$genericPLFunctionDAO = $daoFactory->getGenericPLFunctionDAO();
+		$gqDTOArray = $genericPLFunctionDAO->selectGenericData( $daoFunction, $inputValues );
+
+		$templateVariables['recentBlogProfilesArray'] = $gqDTOArray;
+		$templateVariables['startIndex'] = $startIndex;
+		$templateVariables['length'] = $length; 
+		
+		$profileImageThumbnailHashArray = array();
+		
+		$daoFunction = 'getProfileImage';
+		$inputValues = array();
+
+		$daoFactory = DAOFactory::getInstance();
+		$genericPLFunctionDAO = $daoFactory->getGenericPLFunctionDAO();
+		
+		foreach ( $gqDTOArray as $blogProfile ) {
+			$inputValues[ 'profileID' ] = $blogProfile->get_blogwriter();
+			$gqDTO = $genericPLFunctionDAO->selectGenericData( $daoFunction,  $inputValues );
+			$profileImageThumbnailHashArray[$blogProfile->get_blogwriter()] = $gqDTO->get_output_imagefilehash();
+			eGlooLogger::writeLog( eGlooLogger::$DEBUG, "ViewUpdatedBlogProfiles ImageHash: " . $gqDTO->get_output_imagefilehash() );
+		}
+		
+		$templateVariables['profileImageThumbnailHashArray'] = $profileImageThumbnailHashArray;
+		
+		$blogInfoArray = array();
+		
+		$daoFunction = 'getBlog';
+		$inputValues = array();
+		$daoFactory = DAOFactory::getInstance();
+		$genericPLFunctionDAO = $daoFactory->getGenericPLFunctionDAO();
+		
+		foreach ( $gqDTOArray as $blogProfile ) {
+			$inputValues[ 'inputBlogID' ] = $blogProfile->get_blog_id();
+			$blogInfoArray[$blogProfile->get_blog_id()] = $genericPLFunctionDAO->selectGenericData( $daoFunction, $inputValues );
+		}
+		
+		$templateVariables['blogInfoArray'] = $blogInfoArray;
+
+/*		$daoFunction = 'getBlogProfilesMostReply';
+		$inputValues = array();
+		$inputValues[ 'profile_count' ] = '6';
+		
+		$daoFactory = DAOFactory::getInstance();
+		$genericPLFunctionDAO = $daoFactory->getGenericPLFunctionDAO();
+		$gqDTOArray = $genericPLFunctionDAO->selectGenericData( $daoFunction, $inputValues );
+		
+		$templateVariables['mostReplyBlogProfilesArray'] = $gqDTOArray; */
+		
+        $templateDirector->setTemplateVariables( $templateVariables );            
+        $globalMenuBarContent = new GlobalMenuBarContentProcessor();
+        $templateDirector->setContentProcessors( array( $globalMenuBarContent ) );
+        $output = $templateDirector->processTemplate();
+        header("Content-type: text/html; charset=UTF-8");
+
+        echo $output;
+    }
+    
+    /*
+    		$daoFunction = 'getProfileName';
+			$inputValues = array();
+ 	    	$inputValues[ 'profileID' ] = $viewingProfileID;
+ 	    	 	    	
+ 	    	$daoFactory = DAOFactory::getInstance();
+			$genericPLFunctionDAO = $daoFactory->getGenericPLFunctionDAO();
+			$gqDTO = $genericPLFunctionDAO->selectGenericData( $daoFunction,  $inputValues );
+            $viewingUserProfileName = $gqDTO->get_output_profile_name();
+    */
+}
+
+?>
---
+++ trunk/PHP/Classes/RequestProcessing/Ranking/RankElementInstanceRequestProcessor.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * RankElementInstanceRequestProcessor Class File
+ *
+ * Needs to be commented
+ * 
+ * Copyright 2008 eGloo, LLC
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *  
+ * @author Keith Buel
+ * @copyright 2008 eGloo, LLC
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ * @package RequestProcessing
+ * @version 1.0
+ */
+
+/**
+ * RankElementInstanceRequestProcessor
+ * 
+ * Needs to be commented
+ * 
+ * @package RequestProcessing
+ * @subpackage RequestProcessors
+ */
+class RankElementInstanceRequestProcessor extends RequestProcessor {
+ 	
+ 	    public function processRequest() {
+ 	    	
+ 	    	//who am i?
+ 	    	$rankerProfileID = $_SESSION['MAIN_PROFILE_ID'];
+ 	    	
+ 	    	//the element instance i am ranking
+ 	    	$elementID = $this->requestInfoBean->getGET('elmInstance');
+ 	    	
+ 	    	//the rank I'm ranking it
+ 	    	$elementRank = $this->requestInfoBean->getGET('rank');
+	    	
+ 	    	$daoFunction = 'rankElement';
+ 	    	
+ 	    	$inputValues = array();
+ 	    	$inputValues[ 'profileID' ] = $rankerProfileID;
+ 	    	$inputValues[ 'elementID' ] = $elementID;
+ 	    	$inputValues[ 'rank' ] = $elementRank;
+ 	    	 	    	
+ 	    	$daoFactory = DAOFactory::getInstance();
+			$genericPLFunctionDAO = $daoFactory->getGenericPLFunctionDAO();
+			$gqDTO = $genericPLFunctionDAO->selectGenericData( $daoFunction,  $inputValues );
+			
+			$success = $gqDTO->get_output_successful();
+			if( $success ){
+				eGlooLogger::writeLog( eGlooLogger::$DEBUG, "SUCCESSFUL call to $daoFunction");
+			} else {
+				eGlooLogger::writeLog( eGlooLogger::$DEBUG, "FAILURE call to $daoFunction");
+			}
+			
+ 	    }
+ 	    
+ }
+?>
