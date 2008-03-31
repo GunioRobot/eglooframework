dedd0a642e1ab1e61378219783d462c789b33ff1
egd0000 <egd0000@caf7fd9c-7546-0410-8e03-b3d1908ba6f3>
2008-03-31 08:21:15 +0000

    Committing initial revisions of search request processors
    git-svn-id: http://eglooframework.svn.sourceforge.net/svnroot/eglooframework@89 caf7fd9c-7546-0410-8e03-b3d1908ba6f3

---
+++ trunk/PHP/Classes/RequestProcessing/Search/SearchPeopleBaseRequestProcessor.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * SearchPeopleBaseRequestProcessor Class File
+ *
+ * Needs to be commented
+ * 
+ * Copyright 2008 eGloo, LLC
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *  
+ * @author George Cooper
+ * @copyright 2008 eGloo, LLC
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ * @package RequestProcessing
+ * @version 1.0
+ */
+
+/**
+ * SearchPeopleBaseRequestProcessor
+ * 
+ * Needs to be commented
+ * 
+ * @package RequestProcessing
+ * @subpackage RequestProcessors
+ */
+class SearchPeopleBaseRequestProcessor extends RequestProcessor {
+    private $_templateDefault = '../Templates/Frameworks/Common/XMLHTTPResponse/Search/SearchByName/SearchByNameXMLHTTPResponse.xmltpl';
+
+    public function processRequest() {
+        $this->_templateEngine = new TemplateEngine( 'dev', 'us' );
+
+		$searchParam = $this->requestInfoBean->getGET('name');
+
+		//get a specific blog        	
+        $daoFunction = 'getSearchProfiles';
+		$inputValues = array();
+ 	    $inputValues[ 'name' ] = $searchParam;
+ 	    $inputValues[ 'profile_count' ] = '30';
+ 	    $inputValues[ 'start_index' ] = '0';
+ 	    	 	    	
+ 	    $daoFactory = DAOFactory::getInstance();
+		$genericPLFunctionDAO = $daoFactory->getGenericPLFunctionDAO();
+		$gqDTOArray = $genericPLFunctionDAO->selectGenericData( $daoFunction,  $inputValues );
+		
+		//TODO: do we need this for right now? can we simplify for this release?
+        $searchResults['People']['ResultCount'] = count( $gqDTOArray );
+        $searchResults['People']['Results'] = array( $gqDTOArray );
+
+        $this->_templateEngine->assign( 'searchResults', $searchResults );
+
+        $output = $this->_templateEngine->fetch( $this->_templateDefault );
+
+        header("Content-type: text/html; charset=UTF-8");
+        
+        echo $output;
+
+    }
+}
+
+?>
