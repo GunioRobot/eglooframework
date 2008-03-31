1f145713eb4f91b3c37cc2cf16b827a674d4941d
egd0000 <egd0000@caf7fd9c-7546-0410-8e03-b3d1908ba6f3>
2008-03-31 07:47:15 +0000

    Committing initial revisions of Message request processors
    git-svn-id: http://eglooframework.svn.sourceforge.net/svnroot/eglooframework@82 caf7fd9c-7546-0410-8e03-b3d1908ba6f3

---
+++ trunk/PHP/Classes/RequestProcessing/Message/ViewMessageRequestProcessor.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * ViewMessageRequestProcessor Class File
+ *
+ * Needs to be commented
+ * 
+ * Copyright 2008 eGloo, LLC
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *  
+ * @author George Cooper
+ * @copyright 2008 eGloo, LLC
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ * @package RequestProcessing
+ * @version 1.0
+ */
+
+/**
+ * ViewMessageRequestProcessor
+ * 
+ * Needs to be commented
+ * 
+ * @package RequestProcessing
+ * @subpackage RequestProcessors
+ */
+class ViewMessageRequestProcessor extends RequestProcessor {
+    // TODO SANITY CHECKs -- Do not assume RequestProcessor wasn't drunk when it sent this to you
+    private static $_template = 'blogEntry_default.tpl';
+    
+    public function processRequest() {
+        $this->_templateEngine = new TemplateEngine( 'dev', 'us' );
+
+        $entryList = TestValueConstructor::getBlogEntryList();
+        
+        $this->_templateEngine->assign( 'messageEntryList', $entryList );
+
+        $this->_templateEngine->display( '../Templates/Frameworks/Common/XHTML/Blog/Lists/BlogEntryListContainer.tpl' );
+    }
+    
+}
+
+?>
