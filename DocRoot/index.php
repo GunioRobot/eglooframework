b208b9b8839cec7fcd26cfe8d36d128631d9f296
George Cooper <gcooper@portero.com>
2009-07-22 00:02:54 -0400

    Test

--- DocRoot/index.php
+++ DocRoot/index.php
@@ -49,6 +49,7 @@
     //This is to get IE to accept cookies with its new security model
     //TODO move session management to a decorator so javascript/css calls don't have sessions...
 	setcookie(session_name(), session_id(), time()+60*60*24*30, '/');
+
     /**
      * 1) create a request info bean to be filled by the request validator,
      * 	  if the request is valid
