f1efa6223874afd74c22f2d1f41840ec474af03c
George Cooper <gcooper@portero.com>
2009-11-18 14:08:20 -0500

    Changed eGlooLogger to use consts for constants instead of static members Set it up so that eGlooLogger now handles global errors directly, rather than using external handlers

--- PHP/Classes/Utilities/eGlooLogger.php
+++ PHP/Classes/Utilities/eGlooLogger.php
@@ -45,28 +45,28 @@
  */
 final class eGlooLogger {
     // Class Constants
-    public static $EMERGENCY = 0x80;     // 1000 0000
-    public static $ALERT = 0x40;         // 0100 0000
-    public static $CRITICAL = 0x20;      // 0010 0000
-    public static $ERROR = 0x10;         // 0001 0000
-    public static $WARN = 0x8;           // 0000 1000
-    public static $NOTICE = 0x4;         // 0000 0100
-    public static $INFO = 0x2;           // 0000 0010
-    public static $DEBUG = 0x1;          // 0000 0001
+    const EMERGENCY		= 0x80;		// 1000 0000
+    const ALERT			= 0x40;		// 0100 0000
+    const CRITICAL		= 0x20;		// 0010 0000
+    const ERROR			= 0x10;		// 0001 0000
+    const WARN			= 0x8;		// 0000 1000
+    const NOTICE		= 0x4;		// 0000 0100
+    const INFO			= 0x2;		// 0000 0010
+    const DEBUG			= 0x1;		// 0000 0001
 
-    public static $LOG_OFF = 0x0;        // Do Not Log
-    public static $PRODUCTION = 0xf8;    // Warning or higher
-    public static $STAGING = 0xfe;       // Info or higher
-    public static $DEVELOPMENT = 0xff;   // Log all
+    const LOG_OFF		= 0x0;		// Do Not Log
+    const PRODUCTION	= 0xf8;		// Warning or higher
+    const STAGING		= 0xfe;		// Info or higher
+    const DEVELOPMENT	= 0xff;		// Log all
 
     // Packages
-    public static $SESSION = 'Session';
-    public static $JAVASCRIPT = 'Javascript';
-    public static $CSS = 'CSS';
+    const SESSION		= 'Session';
+    const JAVASCRIPT	= 'Javascript';
+    const CSS			= 'CSS';
 
-	public static $LOG_LOG = "log";		// write to error.log
-	public static $LOG_HTML = "html";	// write to error.html
-	public static $LOG_XML = "xml";		// write to error.xml
+	const LOG_LOG		= "log";	// write to error.log
+	const LOG_HTML		= "html";	// write to error.html
+	const LOG_XML		= "xml";	// write to error.xml
 
     // Attributes
     private static $loggingLevel;
@@ -80,10 +80,10 @@ final class eGlooLogger {
 		self::setLoggingLevel( $level );
 		self::setLoggingType( $format );
 
-		set_error_handler( 'default_error_handler' );
-		set_exception_handler( 'default_exception_handler' );
+		set_error_handler( array('eGlooLogger', 'default_error_handler') );
+		set_exception_handler( array('eGlooLogger', 'default_exception_handler') );
 
-        self::writeLog( self::$INFO, 'Logger Started [Mode: DEVELOPMENT]', 'Logger' );
+        self::writeLog( self::INFO, 'Logger Started [Mode: DEVELOPMENT]', 'Logger' );
 	}
 
     public static function setLoggingLevel( $level ) {
@@ -92,12 +92,12 @@ final class eGlooLogger {
         $num = mt_rand ( 0, 0xffffff );
         self::$requestID = sprintf ( "%06x" , $num );
 
-        self::$logLevelStrings = array( self::$EMERGENCY => "EMERGENCY", self::$ALERT => "ALERT",
-                                        self::$CRITICAL => "CRITICAL",   self::$ERROR => "ERROR",
-                                        self::$WARN => "WARNING",        self::$NOTICE => "NOTICE",
-                                        self::$INFO => "INFO",           self::$DEBUG => "DEBUG",
-                                        self::$LOG_OFF => "OFF",         self::$PRODUCTION => "DEPLOYMENT",
-                                        self::$STAGING => "TESTING",     self::$DEVELOPMENT => "DEVELOPMENT" );
+        self::$logLevelStrings = array( self::EMERGENCY => "EMERGENCY", self::ALERT => "ALERT",
+                                        self::CRITICAL => "CRITICAL",   self::ERROR => "ERROR",
+                                        self::WARN => "WARNING",        self::NOTICE => "NOTICE",
+                                        self::INFO => "INFO",           self::DEBUG => "DEBUG",
+                                        self::LOG_OFF => "OFF",         self::PRODUCTION => "DEPLOYMENT",
+                                        self::STAGING => "TESTING",     self::DEVELOPMENT => "DEVELOPMENT" );
     }
     
     
@@ -113,7 +113,7 @@ final class eGlooLogger {
      	//log is the default
      	
      	//Create the HTML headers, and change the logfile to an HTML file.
-     	if($type == self::$LOG_HTML){
+     	if($type == self::LOG_HTML){
      		self::$logFilePath = self::$htmlFilePath;
      		$header = '<html><body BGCOLOR="#000000">';
      		if ( (file_put_contents( self::$logFilePath, $header . "\n", FILE_APPEND ) ) === false ) {
@@ -123,7 +123,7 @@ final class eGlooLogger {
 
      	//TODO make an XML schema for this thang
      	//DO NOT use yet. in fact, I'll just go ahead and comment it out...
-     	/*if($type == self::$LOG_XML){
+     	/*if($type == self::LOG_XML){
      		self::$logFilePath = 'error.xml';
      	}*/
 
@@ -157,7 +157,7 @@ final class eGlooLogger {
             }
             
             //Default, write to error.log
-        	if( self::$loggingType == self::$LOG_LOG){
+        	if( self::$loggingType == self::LOG_LOG){
 //            	if ( (file_put_contents( self::$logFilePath, $message . "\n", FILE_APPEND ) ) === false ) {
 //               		throw new eGlooLoggerException( 'Error writing to log' );
 //            	}
@@ -167,11 +167,11 @@ final class eGlooLogger {
             }
             
             //If HTML, write correct headers
-            if( self::$loggingType == self::$LOG_HTML){
+            if( self::$loggingType == self::LOG_HTML){
             	
 	            //if Emergency, Alert, Critical, or Error
 	            //write in red
-	            if($level & self::$EMERGENCY OR $level & self::$ALERT OR $level & self::$CRITICAL OR $level & self::$ERROR){
+	            if($level & self::EMERGENCY OR $level & self::ALERT OR $level & self::CRITICAL OR $level & self::ERROR){
 	            	$message = '<font color="#FF0000">'.$message.'</font></body></html>';
 	            	//print $message;
 	            	if ( (file_put_contents( self::$logFilePath, $message . "<BR><BR>", FILE_APPEND ) ) === false ) {
@@ -181,7 +181,7 @@ final class eGlooLogger {
 	            
 	            //if Warning or Notice
 	            //write in yellow
-	            if( $level & self::$NOTICE OR $level & self::$WARN){
+	            if( $level & self::NOTICE OR $level & self::WARN){
 	            	$message = '<font color="#FFFF00">'.$message.'</font></body></html>';
       	            if ( (file_put_contents( self::$logFilePath, $message . "<BR><BR>", FILE_APPEND ) ) === false ) {
 	                	throw new eGlooLoggerException( 'Error writing to log' );
@@ -190,7 +190,7 @@ final class eGlooLogger {
 	            
 	            //if Debug or Info
 	            //write in green
-	            if($level & self::$INFO OR $level & self::$DEBUG){
+	            if($level & self::INFO OR $level & self::DEBUG){
 	            	$message = '<font color="#00FF00">'.$message.'</font></body></html>';
           	        if ( (file_put_contents( self::$logFilePath, $message . "<BR><BR>", FILE_APPEND ) ) === false ) {
 	                	throw new eGlooLoggerException( 'Error writing to log' );
@@ -203,27 +203,74 @@ final class eGlooLogger {
         }//if correct logging level
     }
 
+	/**
+	 * Defines the default exception runtime handler.
+	 * 
+	 * Long Description Goes Here
+	 * 
+	 * @param Exception $exception exception thrown
+	 * @todo Finish commenting
+	 */
+	public static function default_exception_handler( $exception ) {
+		self::writeLog( self::EMERGENCY, 
+			'[File: ' . $_SERVER['SCRIPT_NAME'] . 
+			' Line: ' . __LINE__ . '] Programmer ' .
+			'Error: Request Handler Threw Unknown Exception' .
+			'\n\t' . $exception->getMessage() );    
+
+	    exit;
+	}
+
+	/**
+	 * Defines the default error runtime handler.
+	 * 
+	 * Long Description Goes Here
+	 * 
+	 * @todo Finish commenting
+	 */
+	public static function default_error_handler( $severity, $message, $filename, $linenum, $errcontext ) {
+	    $killRequest = false;
+
+	    switch( $severity ) {
+	        case E_USER_NOTICE :
+	            $levelString = 'USER NOTICE';
+	            break;
+	        case E_USER_WARNING :
+	            $levelString = 'USER WARNING';
+	            break;
+	        case E_USER_ERROR :
+	            $levelString = 'USER ERROR';
+	            $killRequest = true;
+	            break;
+	        case E_ERROR :
+	            $levelString = 'RUN-TIME ERROR';
+	            $killRequest = true;
+	            break;        
+	        case E_WARNING :
+	            $levelString = 'RUN-TIME WARNING';     // Non-fatal error
+	            break;
+	        case E_NOTICE :
+	            $levelString = 'RUN-TIME NOTICE';      // Could indicate an error
+	            break;
+	        case E_RECOVERABLE_ERROR :
+	            $levelString = 'RECOVERABLE ERROR';    // Recoverable error
+	            break;
+	        case E_STRICT :
+	            $levelString = 'STRICT NOTICE';        // Could indicate an error
+	            break;
+	        default :
+	            $levelString = 'UNKNOWN';
+	            break;
+	    }
+
+		self::writeLog( self::EMERGENCY, $levelString . ' ' . 
+		                              $message . ' ' . $filename . ' ' . $linenum );            
+
+        if ( $killRequest === true ) {
+            exit;
+        }
+
+	}
+
 }
 
-/**
- * Private exception subclass for use by eGlooLogger
- */
-final class eGlooLoggerException extends Exception {
-
-   /**
-    * eGlooLoggerException constructor.  Takes a message and a code and invokes
-    * the parent (Exception) constructor.  May eventually contain additional code,
-    * but for now acts as a means of determining the exact type of exception thrown
-    * so it is possible to track down what threw it.
-    *
-    * @param $message   the message that this exception will contain
-    * @param $code      the optional code of this exception (unused)
-    * @returns          an eGlooLoggerException
-    */
-   public function __construct( $message, $code = 0 ) {
-       // Call parent constructor
-       parent::__construct( $message, $code );
-   }
-}
-
-?>
