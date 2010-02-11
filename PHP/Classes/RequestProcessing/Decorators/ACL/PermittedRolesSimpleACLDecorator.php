9ae43ca99838ea4179b00fae49308728603b8df8
George Cooper <gcooper@portero.com>
2010-02-11 15:47:21 -0500

    Making a base ACL decorator system for eGloo

---
+++ PHP/Classes/RequestProcessing/Decorators/ACL/PermittedRolesSimpleACLDecorator.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * PermittedRolesSimpleACLDecorator Class File
+ *
+ * Needs to be commented
+ * 
+ * Copyright 2010 eGloo, LLC
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *  
+ * @author George Cooper
+ * @copyright 2010 eGloo, LLC
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ * @package RequestProcessing
+ * @version 1.0
+ */
+
+/**
+ * PermittedRolesSimpleACLDecorator
+ * 
+ * Needs to be commented
+ * 
+ * @package RequestProcessing
+ * @subpackage Decorators
+ */
+class PermittedRolesSimpleACLDecorator extends SimpleACLDecorator {
+
+	protected $_permittedRoles;
+
+   /**
+    * do any pre processing here
+    */
+	protected function requestPreProcessing() {
+		eGlooLogger::writeLog( eGlooLogger::DEBUG, "PermittedRolesSimpleACLDecorator::requestPreProcessing", 'Decorators' );
+		$retVal = false;
+
+		$retVal = $this->__checkPermittedRoles();
+
+		if (!$retVal) {
+			$this->__actOnAccessDenied();
+		}
+
+		return $retVal;
+	}
+
+   /**
+    * do any post processing here
+    */
+	protected function requestPostProcessing(){
+		
+	}
+
+	protected function __checkPermittedRoles() {
+		$retVal = false;
+
+		if (isset($_SESSION['egACLRole']) && in_array($_SESSION['egACLRole'], $this->_permittedRoles)) {
+			$retVal = true;
+		}
+
+		return $retVal;
+	}
+
+	protected function __actOnAccessDenied() {
+		
+	}
+
+}
+
