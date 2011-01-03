d6b6a722af2f798a98868d1ee44cc3eabf6760ea
George Cooper <gcooper@portero.com>
2011-01-03 00:16:48 -0500

    Screwing around with CRUD on registrations, but I'm probably going to make this stored procedures for my own sanity.

--- PHP/Classes/DataProcessing/Persistence/DataAccessObjects/Registration/PGSQLRegistrationDAO.php
+++ PHP/Classes/DataProcessing/Persistence/DataAccessObjects/Registration/PGSQLRegistrationDAO.php
@@ -39,6 +39,34 @@
 class PGSQLRegistrationDAO extends RegistrationDAO {
 
 	public function CRUDCreate( $formDTO ) {
+		$db_handle = DBConnectionManager::getConnection()->getRawConnectionResource();
+
+		$username = $formDTO->getUsername();
+		$username_id = null;
+
+		if ( !$this->usernameAvailable( $username ) ) {
+			echo_r('Username not available');
+		} else {
+			echo_r('Username available');
+			// $username_id = $this->createUsername( $username );
+			
+			// if ($username_id !== false && $username_id !== null) {
+			// 	echo_r('Username creation success: ' . $username_id);
+			// } else {
+			// 	echo_r('Failure: ');
+			// 	echo_r($username_id);
+			// }
+		}
+die;
+		//Prepare a query for execution
+		$result = pg_prepare($db_handle, "query", 'INSERT INTO profilelayout (profile_id, element_id, layoutcolumn, layoutrow ) VALUES ($1, $2, $3, $4)');
+
+		// Execute the prepared query.	Note that it is not necessary to escape
+		$result = pg_execute($db_handle, "query", array($profileID, $cubeID, $column, $row ));
+
+		pg_close( $db_handle );		
+
+		echo_r($formDTO);
 		die_r('here2');
 	}
 
@@ -53,6 +81,65 @@ class PGSQLRegistrationDAO extends RegistrationDAO {
 	public function CRUDDestroy( $formDTO ) {
 		
 	}
+
+	public function usernameAvailable( $username ) {
+		$retVal = false;
+
+		//get handle
+		$db_handle = DBConnectionManager::getConnection()->getRawConnectionResource();
+
+		//Prepare a query for execution
+		$result = pg_prepare($db_handle, 'usernameAvailable', 'SELECT username_id FROM usernames WHERE username = $1 LIMIT 1');
+
+		// Execute the prepared query.	Note that it is not necessary to escape
+		$result = pg_execute($db_handle, 'usernameAvailable', array($username));
+
+		$resultValues = pg_fetch_assoc( $result );
+		$username_id = $resultValues['username_id'];
+
+		if ( $username_id === null || $username_id === false ) {
+			$retVal = true;
+		} else {
+			$retVal = false;
+		}
+
+		// Close connection
+		pg_close( $db_handle );
+
+		return $retVal;
+	}
+
+	public function createUsername( $username ) {
+		$retVal = false;
+
+		// Get a connection
+		$db_handle = DBConnectionManager::getConnection()->getRawConnectionResource();
+
+		//Prepare a query for execution
+		$result = pg_prepare( $db_handle, 'insertUsername', 'INSERT INTO usernames ( active, username ) VALUES ( $1, $2 )' );
+
+		// Execute the prepared query.	Note that it is not necessary to escape
+		$result = pg_execute( $db_handle, 'insertUsername', array( 'false', $username ) );
+		
+		if ( $result !== false ) {
+			// Get the insert ID
+			$result = pg_prepare( $db_handle, 'getLastUsernameID', 'SELECT currval(\'seq_usernames_username_id\') AS username_id' );
+			$result = pg_execute( $db_handle, 'getLastUsernameID', array() );
+
+			$resultValues = pg_fetch_assoc( $result );
+			$username_id = $resultValues['username_id'];
+
+			$retVal = $username_id !== false && $username_id !== null ? $username_id : false;
+		} else {
+			$retVal = $result;
+		}
+
+		// Close this connection
+		pg_close( $db_handle );
+
+		return $retVal;
+	}
+
 
 }
 
