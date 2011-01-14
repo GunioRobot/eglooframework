efc9da9e3f11fd16749f536a690c95ca152dc8eb
George Cooper <egd0000@egloo.com>
2011-01-14 12:31:28 -0500

    You know that one yearbook photo of you with really long hair you want to forget?  Yea, this is the code equivalent

---
+++ Database/Plugins/CDN/CloudFront/PostgreSQLDB/Build01.sql
@@ -0,0 +1,22 @@
+/*
+cloudfront_upload_queue
+	item_id (pk)
+	bucket_name
+	distribution_url
+	image_cache_namespace
+	image_cache_id
+	image_cache_name
+	inserted_at
+	local_file_path
+	upload_file_path
+
+cloudfront_files
+	file_id (pk)
+	bucket_name
+	distribution_url
+	image_cache_namespace
+	image_cache_id
+	image_cache_name
+	created_at
+	local_file_path
+*/
