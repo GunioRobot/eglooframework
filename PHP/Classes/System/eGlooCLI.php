65f66e60fdc9263e9196eb465b950536ee9743f8
George Cooper <egd0000@egloo.com>
2011-06-03 12:26:23 -0400

    Allowing eGloo to take additional class paths at CLI

--- PHP/Classes/System/eGlooCLI.php
+++ PHP/Classes/System/eGlooCLI.php
@@ -40,6 +40,10 @@ class eGlooCLI {
 
 	public static $verbose = false;
 
+	public static $script_arguments = array();
+
+	public static $class_paths = null;
+
 	// TODO make a default for this so it doesn't freak out
 	public static function execute( $command = null, $arguments = null ) {
 		switch( $command ) {
@@ -569,6 +573,47 @@ class eGlooCLI {
 	public static function printVersionInfo() {
 		// TODO grab this from somewhere more sensible
 		echo 'Version 1.0 Developer Preview 2' ."\n";
+	}
+
+	public static function setScriptArguments( $script_arguments ) {
+		if ( is_array($script_arguments) ) {
+			self::$script_arguments = $script_arguments;
+		}
+	}
+
+	public static function getScriptArguments() {
+		$retVal = null;
+
+		$retVal = self::$script_arguments;
+
+		return $retVal;
+	}
+
+	public static function getClassPaths() {
+		$retVal = array();
+
+		if ( self::$class_paths === null ) {
+			foreach( self::$script_arguments as $argument_key => $argument_value ) {
+				$matches = array();
+				preg_match('/^--class-path=([a-zA-Z0-9\/_.~: -]+)$/', $argument_value, $matches);
+
+				if ( !empty($matches) && isset($matches[1]) ) {
+					$full_path = realpath( $matches[1] );
+
+					if ( file_exists($full_path) && is_dir($full_path) && is_readable($full_path) ) {
+						$retVal[] = $full_path;
+					} else {
+						echo 'Provided path "' . $full_path . '" is not a valid path.  Ignoring...' . "\n";
+					}
+				}
+			}
+
+			self::$class_paths = $retVal;
+		} else {
+			$retVal = self::$class_paths;
+		}
+
+		return $retVal;
 	}
 
 }
